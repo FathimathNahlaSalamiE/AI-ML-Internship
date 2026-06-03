0e9194e729a47f01283792ba78b099db932af2a3
Fathimath Nahla Salami E <nahlasalami321@gmail.com>
2026-06-03 12:57:35 +0530

    day 40 completed

---
+++ day 40/task2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+data = {
+    "Hours": [1, 2, 3, 5, 6, 7],
+    "Result": [0, 0, 0, 1, 1, 1]
+}
+df = pd.DataFrame(data)
+X = df[["Hours"]]
+y = df["Result"]
+model = DecisionTreeClassifier()
+model.fit(X, y)
+prediction = model.predict([[4]])
+print(prediction)
+prediction = model.predict([[2], [5], [8]])
+print(prediction)
